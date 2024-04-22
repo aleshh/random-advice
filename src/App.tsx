8e83fb26e2be1ca6615b06643e54ae2177819f35
Alesh Houdek <mail@alesh.com>
2024-04-21 21:35:08 -0600

    Add some quotes and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import "./App.css";
+import quotes from "./quotes";
+
+function randomPrettyColor() {
+  const hue = Math.floor(Math.random() * 256);
+  return `hsl(${hue}, 93%, 84%)`;
+}
+
+function setBodyStyle() {
+  const body = document.querySelector("body");
+
+  if (body) {
+    body.style.background = `linear-gradient(${randomPrettyColor()}, ${randomPrettyColor()}`;
+  }
+}
 
 function App() {
+  const index = Math.floor(Math.random() * quotes.length);
+  const quote = quotes[index];
+  setBodyStyle();
+
   return (
     <>
       <main>
-        <p>Hello</p>
+        <p className="quote">{quote.text}</p>
+        <p className="link">
+          <a href={quote.source}>#</a>
+        </p>
       </main>
     </>
   );
